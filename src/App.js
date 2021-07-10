70999705296cb0bd07af77bc6776b80da0feab3f
Omotuyole Olawale Olamide <Horlarmihdeh@gmail.com>
2021-07-10 21:22:35 +0100

    added basic and common styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Register from './components/Register/register'
 import Login from './components/Login/login'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/home/home'
+import Header from './components/header/header'
 
 
 const App = () => {
   return (
     <Router>
-      <div className="app">
+      <div className="app" style={{background: "black"}}>
+        <Header />
         <Home />
         {/* <Register />
         <Login /> */}
