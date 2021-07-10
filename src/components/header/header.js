f08b1e62a02641c33633d9842202ff7621ccc782
Omotuyole Olawale Olamide <Horlarmihdeh@gmail.com>
2021-07-10 21:23:13 +0100

    header componenet

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './header.css'
 
+import {  NavLink } from 'react-router-dom'
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header__logo">
+                The <span>Mhuvies</span>
+            </div>
+            <ul>
+                <li><NavLink to="#"><i className="fas fa-sign-in-alt"></i>Login</NavLink></li>
+                <li><NavLink to="#"><i className="fas fa-user-plus"></i>Register</NavLink></li>
+            </ul>
         </div>
     )
 }
