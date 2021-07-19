d1bdbe87937fbf4cd9733e2e311340bf87029099
Omotuyole Olawale Olamide <Horlarmihdeh@gmail.com>
2021-07-19 16:24:48 +0100

    login pages

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,35 +1,91 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import axios from 'axios';
-
+import { validateEmail } from '../../function/helper';
+import './login.css'
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router-dom';
+import { userContext } from '../../context/userContext';
 
 const Login = () => {
-    
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('')
-    const login =()=>{
+    const history = useHistory()
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [emailErr, setEmailErr] = useState('')
+    const [passwordErr, setPasswordErr] = useState('')
+    const [, setUser] = useContext(userContext)
+
+    const login = async(e)=>{
+        e.preventDefault()
+        resetFields()
+        if(email.trim().length <= 0){
+            return setEmailErr('Email field is required')
+          }
+          if(validateEmail(email) === false){
+            return setEmailErr("Kindly input a valid email")
+          }
+          if(password.trim().length <= 0){
+            return setPasswordErr('Kindly input your password')
+          }
+          if(password.trim().length <= 5){
+            return setPasswordErr('Password must be 6 characters or more')
+          }
+          setLoading(true)
+          try {
+            const res = await axios({
+                method: "POST",
+                data: {
+                    email,
+                    password
+                },
+                url: "http://localhost:8000/user"
+            })
+            if(res.status !== '201' || res.status !== '200'){
+                throw Error('Could not created account')
+            }else{
+                setUser(res.data.email)
+                localStorage.setItem('user')
+                setLoading(false)
+                history.goBack()
+            }
+
+          } catch (error) {
+              if(error.status === '404'){
+                  setLoading(false)
+                  toast('Server Error, try again')
+
+              }
+          }
         
-        axios({
-            method: "POST",
-            data: {
-                name,
-                password
-            },
-            withCredentials: true,
-            url: "http://localhost:4000/login"
-        })
-        .then(res=> console.log(res))
         
     }
+    const resetFields = ()=>{
+        setEmailErr('')
+        setPasswordErr('')
+    }
     return (
-        <div>
-            <h2>Login your details</h2>
-            <label>Username:</label>
-            <input type="text"  onChange={(e)=>{setName(e.target.value)}} placeholder="Enter your username" />
-            <label>Password:</label>
-            <input type="password"  onChange={(e)=>{setPassword(e.target.value)}} placeholder="Enter your password" />
-            <button onClick= {login}>Login</button>
+        <div className="login__field">
+              {loading ? (
+                <h4 className="text-warning">loading...please wait.</h4>
+              ) : (<>
+              {<div className="login__box">
+                <h1>Login Here</h1>
+                <form onSubmit={login}>
+                    <div className="form_ctrl">
+                        <label>Username</label>
+                        <input type="email" className={emailErr? 'active': ''} value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Username" />
+                        {emailErr && <small>{emailErr}</small>}
+                    </div>
+                    <div className="form_ctrl">
+                        <label>Password</label>
+                        <input type="password"value={password} className={passwordErr? 'active':''} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" />
+                        {passwordErr && <small>{passwordErr}</small>}
+                    </div>
+                    <input type="submit" value="Login" />
+                </form>
+            </div>}</>)}
         </div>
-    )
+      )
 
 }
 
